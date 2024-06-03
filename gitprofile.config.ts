530ba5dbad65b1addadb3dc0e862b8d2888420e2
Marianne Ray <marianneray14@gmail.com>
2024-06-03 00:28:57 -0400

    Profile updates

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -1,246 +1,233 @@
 // gitprofile.config.ts
 
 const CONFIG = {
-  github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
-  },
-  /**
-   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
-   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
-   */
-  base: '/gitprofile/',
-  projects: {
     github: {
-      display: true, // Display GitHub projects?
-      header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
-      automatic: {
-        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
-        exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      username: 'marianneray', // Your GitHub org/user name. (This is the only required config)
+    },
+    /**
+     * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
+     * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
+     * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
+     */
+    base: '/',
+    projects: {
+      github: {
+        display: false, // Display GitHub projects?
+        header: 'Github Projects',
+        mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+        automatic: {
+          sortBy: 'stars', // Sort projects by 'stars' or 'updated'
+          limit: 8, // How many projects to display.
+          exclude: {
+            forks: false, // Forked projects will not be displayed if set to true.
+            projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          },
+        },
+        manual: {
+          // Properties for manually specifying projects
+          projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
-      manual: {
-        // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      external: {
+        header: 'Various Adventures',
+        // To hide the `External Projects` section, keep it empty.
+        projects: [
+          {
+            title: 'Tufts Energy Conference 2018',
+            description:
+              'As Director of Content, I co-led a team of 20 to put together content addressing what policy, technology, and financial disruptions must occur to transform the global energy paradigm to meet national and international goals in 2030, 2050, and beyond. The Tufts Energy Conference is one of the largest entirely student-run energy conferences in New England, attracting nearly 400 attendees each year.',
+            imageUrl:
+              'https://higherlogicdownload.s3.amazonaws.com/DISTRICTENERGY/UploadedImages/2vqS8MHSSMKH5WmPbKMg_tec-2018-full-banner-logo-2.png',
+            link: 'https://environment.tufts.edu/tec/files/TEC2018_Program.pdf',
+          },
+          {
+              title: 'Tufts Energy Conference 2017',
+              description:
+                'As a member of the content team, I organized a panel examining the advantages and drawbacks of, and decision-making process for, deploying existing renewable technologies versus pursuing the innovation of newer, more cost-effective technologies. The 2017 conference explored the avenues through which innovation can be deployed to expand global energy access, including technology, policy, funding, and partnerships.',
+              imageUrl:
+                'https://environment.tufts.edu/wp-content/uploads/TEC-e1507661460114.png',
+              link: 'https://tufts.qualtrics.com/ControlPanel/File.php?F=F_a3RtPklqK2NLbqR',
+
+          },
+        ],
       },
     },
-    external: {
-      header: 'My Projects',
-      // To hide the `External Projects` section, keep it empty.
-      projects: [
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-      ],
+    seo: {
+      title: 'Portfolio of Ariful Alam',
+      description: '',
+      imageURL: '',
     },
-  },
-  seo: {
-    title: 'Portfolio of Ariful Alam',
-    description: '',
-    imageURL: '',
-  },
-  social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
-    facebook: '',
-    instagram: '',
-    reddit: '',
-    threads: '',
-    youtube: '', // example: 'pewdiepie'
-    udemy: '',
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    website: 'https://www.arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
-  },
-  resume: {
-    fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
-  },
-  skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
-  ],
-  experiences: [
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+    social: {
+      linkedin: 'marianne-ray',
+      //twitter: '',
+      //mastodon: '',
+      //researchGate: '',
+      //facebook: '',
+      //instagram: '',
+      //reddit: '',
+      //threads: '',
+      //youtube: '',
+      //udemy: '',
+      //dribbble: '',
+      //behance: '',
+      //medium: '',
+      //dev: '',
+      //stackoverflow: '',
+      //skype: '',
+      //telegram: '',
+      //website: '',
+      //phone: '',
+      //email: '',
     },
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+    resume: {
+      fileUrl:
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
     },
-  ],
-  certifications: [
-    {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
-    },
-  ],
-  educations: [
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
-    },
-  ],
-  publications: [
-    {
-      title: 'Publication Title',
-      conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
-      journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-  ],
-  // Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
-  },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
-  // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: {
-    id: '',
-    snippetVersion: 6,
-  },
-  themeConfig: {
-    defaultTheme: 'lofi',
-
-    // Hides the switch in the navbar
-    // Useful if you want to support a single color mode
-    disableSwitch: false,
-
-    // Should use the prefers-color-scheme media-query,
-    // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
-
-    // Display the ring in Profile picture
-    displayAvatarRing: true,
-
-    // Available themes. To remove any theme, exclude from here.
-    themes: [
-      'light',
-      'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
-      'dim',
-      'nord',
-      'sunset',
-      'procyon',
+    skills: [
     ],
+    experiences: [
+      {
+          company: 'Ginkgo Bioworks',
+          position: 'Senior Software Engineer',
+          from: 'February 2023',
+          to: 'present',
+          companyLink: 'https://www.ginkgobiosecurity.com/',
+        },
+      {
+        company: 'Broad Institute of MIT & Harvard',
+        position: 'Senior Software Engineer',
+        from: 'August 2020',
+        to: 'January 2023',
+        companyLink: 'https://www.broadinstitute.org/',
+      },
+      {
+        company: 'One Brave Idea',
+        position: 'Software Engineer',
+        from: 'March 2019',
+        to: 'August 2020',
+        companyLink: 'https://www.onebraveidea.org/',
+      },
+      {
+          company: 'Raytheon',
+          position: 'Software Engineer I',
+          from: 'September 2018',
+          to: 'February 2019',
+          companyLink: 'https://www.rtx.com/',
+      },
+    ],
+    certifications: [
+      //{
+      //  name: 'Lorem ipsum',
+      //  body: 'Lorem ipsum dolor sit amet',
+      //  year: 'March 2022',
+      //  link: 'https://example.com',
+      //},
+    ],
+    educations: [
+      {
+        institution: 'MIT Sloan',
+        degree: 'Certificate in Management & Leadership (Executive Education)',
+        from: '2021',
+        to: '2022',
+      },
+      {
+        institution: 'Tufts University',
+        degree: 'BS Geology, Computer Science (minor)',
+        from: '2014',
+        to: '2018',
+      },
+    ],
+    publications: [
+      {
+        title: 'Transmission from vaccinated individuals in a large SARS-CoV-2 Delta variant outbreak',
+        conferenceName: '',
+        journalName: 'Cell',
+        authors: 'Siddle KJ, Krasilnikova LA, Moreno GK, Schaffner SF, Vostok J, Fitzgerald NA, Lemieux JE, Barkas N, Loreth C, Specht I, Tomkins-Tinch CH, Paull JS, Schaeffer B, Taylor BP, Loftness B, Johnson H, Schubert PL, Shephard HM, Doucette M, Fink T, Lang AS, Baez S, Beauchamp J, Hennigan S, Buzby E, Ash S, Brown J, Clancy S, Cofsky S, Gagne L, Hall J, Harrington R, Gionet GL, DeRuff KC, Vodzak ME, Adams GC, Dobbins ST, Slack SD, Reilly SK, Anderson LM, Cipicchio MC, DeFelice MT, Grimsby JL, Anderson SE, Blumenstiel BS, Meldrim JC, Rooke HM, Vicente G, Smith NL, Messer KS, Reagan FL, Mandese ZM, Lee MD, Ray MC, Fisher ME, Ulcena MA, Nolet CM, English SE, Larkin KL, Vernest K, Chaluvadi S, Arvidson D, Melchiono M, Covell T, Harik V, Brock-Fisher T, Dunn M, Kearns A, Hanage WP, Bernard C, Philippakis A, Lennon NJ, Gabriel SB, Gallagher GR, Smole S, Madoff LC, Brown CM, Park DJ, MacInnis BL, Sabeti PC',
+        link: 'https://www.cell.com/cell/fulltext/S0092-8674(21)01490-2',
+        description:
+          'An outbreak of over 1,000 COVID-19 cases in Provincetown, Massachusetts (MA), in July 2021-the first large outbreak mostly in vaccinated individuals in the US-prompted a comprehensive public health response, motivating changes to national masking recommendations and raising questions about infection and transmission among vaccinated individuals. To address these questions, we combined viral genomic and epidemiological data from 467 individuals, including 40% of outbreak-associated cases. The Delta variant accounted for 99% of cases in this dataset; it was introduced from at least 40 sources, but 83% of cases derived from a single source, likely through transmission across multiple settings over a short time rather than a single event. Genomic and epidemiological data supported multiple transmissions of Delta from and between fully vaccinated individuals. However, despite its magnitude, the outbreak had limited onward impact in MA and the US overall, likely due to high vaccination rates and a robust public health response.',
+      },
+      {
+          title: 'Behind the COVID-19 Testing Pipeline',
+          conferenceName: 'Women in Data Science Conference',
+          journalName: '',
+          authors: 'Marianne Ray',
+          link: '',
+          description:
+            '',
+        },
+    ],
+    themeConfig: {
+      defaultTheme: 'lofi',
 
-    // Custom theme, applied to `procyon` theme
-    customTheme: {
-      primary: '#fc055b',
-      secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
-      '--rounded-box': '3rem',
-      '--rounded-btn': '3rem',
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: false,
+
+      // Should use the prefers-color-scheme media-query,
+      // using user system preferences, instead of the hardcoded defaultTheme
+      respectPrefersColorScheme: false,
+
+      // Display the ring in Profile picture
+      displayAvatarRing: true,
+
+      // Available themes. To remove any theme, exclude from here.
+      themes: [
+        'light',
+        'dark',
+        'cupcake',
+        'bumblebee',
+        'emerald',
+        'corporate',
+        'synthwave',
+        'retro',
+        'cyberpunk',
+        'valentine',
+        'halloween',
+        'garden',
+        'forest',
+        'aqua',
+        'lofi',
+        'pastel',
+        'fantasy',
+        'wireframe',
+        'black',
+        'luxury',
+        'dracula',
+        'cmyk',
+        'autumn',
+        'business',
+        'acid',
+        'lemonade',
+        'night',
+        'coffee',
+        'winter',
+        'dim',
+        'nord',
+        'sunset',
+        'procyon',
+      ],
+
+      // Custom theme, applied to `procyon` theme
+      customTheme: {
+        primary: '#fc055b',
+        secondary: '#219aaf',
+        accent: '#e8d03a',
+        neutral: '#2A2730',
+        'base-100': '#E3E3ED',
+        '--rounded-box': '3rem',
+        '--rounded-btn': '3rem',
+      },
     },
-  },
 
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+    // Optional Footer. Supports plain text or HTML.
+    footer: `Made with <a
+        class="text-primary" href="https://github.com/arifszn/gitprofile"
+        target="_blank"
+        rel="noreferrer"
+      >GitProfile</a> and ❤️`,
 
-  enablePWA: true,
-};
+    enablePWA: true,
+  };
 
-export default CONFIG;
+  export default CONFIG;
