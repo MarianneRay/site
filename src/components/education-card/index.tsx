7b5ebf327f2518fa1a6c13e03499bd65b527496f
Marianne Ray <marianneray14@gmail.com>
2024-06-04 19:06:14 -0400

    typo

--- src/components/education-card/index.tsx
+++ src/components/education-card/index.tsx
@@ -16,9 +16,9 @@ const ListItem = ({
       className="absolute w-2 h-2 bg-base-300 rounded-full border border-base-300 mt-1.5"
       style={{ left: '-4.5px' }}
     ></div>
-    (!!time ? <div className="my-0.5 text-xs">{time}</div> : <></>)
-    <div className="font-semibold">{institution}</div>
-    <h3 className="mb-4 font-normal">{degree}</h3>
+    (!!time && <div className="my-0.5 text-xs">{time}</div>)
+    <h3 className="font-semibold">{institution}</h3>
+    <div className="mb-4 font-normal">{degree}</div>
   </li>
 );
 
@@ -74,7 +74,7 @@ const EducationCard = ({
                 {educations.map((item, index) => (
                   <ListItem
                     key={index}
-                    time={!!item.from && !!item.to ?  `${item.from} - ${item.to}`: ``}
+                    time={!!item.from && !!item.to ?  `${item.from} - ${item.to}`: undefined}
                     degree={item.degree}
                     institution={item.institution}
                   />
